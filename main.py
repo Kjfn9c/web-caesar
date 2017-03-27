85f89003cba975093937c74d93a8845014c3d85f
Kyle Finnegan <Kyle Finnegan>
2017-03-26 19:04:51 -0500

    finished project

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 #
 import webapp2
 #import caesar
+import cgi
 key_lower = 'abcdefghijklmnopqrstuvwxyz'
 key_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -48,16 +49,35 @@ def encrypt(text, rot):
             turing += char
     return turing
 
+def build_page(texrarea_content):
+        rot_label = "<label>Rotate by:</label>"
+        rotation_input = "<input type='number' name='rotation'/>"
+
+        message_label = "<label>Type a Message:</label>"
+        textarea = "<textarea name='message'>" + texrarea_content + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = ("<form method='post'>" +
+            rot_label + rotation_input + "<br>" +
+            message_label + textarea + "<br>" +
+            submit + "</form>")
+
+        header = "<h2>Web Caesar</h2>"
+
+        return header + form
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        content = build_page("")
+        self.response.write(content)
 
-        message = "Hello, world!"
-        encrypted_message = encrypt(message, 13)
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get("rotation"))
+        encrypted_message = encrypt(message, rotation)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
+        self.response.write(content)
 
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
-        submit = "<input type='submit'/>"
-        form = "<form>" + textarea + "<br>" + submit + "</form>"
-        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
